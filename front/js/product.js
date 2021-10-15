25ffbdad34152c98fef29ce62493ca3bc35f016a
OliverTwist1234 <olivehub@gmail.com>
2021-10-15 15:22:32 +0200

    Récupération valeurs sélectionnées utilisateur dans page produit et stockage dans le local storage.

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,11 @@
-//Fonction globale qui va récupèrer l'ID du canapé selectionné et l'afficher dans la page product.
+//Fonction globale qui va récupèrer l'ID du canapé selectionné et l'afficher dans la page product.html
 (async function () {
   const canapeId = getCanapeId();
   console.log(canapeId);
   const canap = await apiCanape(canapeId);
   console.log(canap);
   affichCanap(canap);
+  recupUserSelect(canap);
 })();
 
 // Fonction de récupération du l'ID via le lien
@@ -19,7 +20,7 @@ function apiCanape(canapeId) {
     .then(function (res) {
       if (res.ok) {
         return res.json();
-      } 
+      }
     })
     .then(function (canapes) {
       return canapes;
@@ -43,4 +44,63 @@ function affichCanap(canap) {
       "colors"
     ).innerHTML += `<option value="${canap.colors[i]}">${canap.colors[i]}</option>`;
   }
+}
+
+/*Fonction de récupération des valeurs sélectionnées par l'utilisateur dans un 
+objet puis dans le localstorage */
+function recupUserSelect(canap) {
+  const ajoutPanier = document.getElementById("addToCart");
+  ajoutPanier.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    //récupération des valeurs sélectionnées
+    let colorProduit = document.getElementById("colors").value;
+    let quantityProduit = document.getElementById("quantity").value;
+
+    //Groupement des valeurs dans un objet
+    const userSelectProduit = {
+      nomProduit: canap.name,
+      idProduit: canap._id,
+      couleur: colorProduit,
+      quantite: quantityProduit,
+      prix: canap.price.toFixed(2),
+    };
+
+    //Stockage des valeurs sélectionnées dans le local storage
+
+    //Déclaration variable "produitDansLocalStorage" pour key et values dans le local storage
+    let produitDansLocalStorage = JSON.parse(localStorage.getItem("produit"));
+    //JSON.parse pour convertir les données au format JSON en JS dans locol storage
+
+    //fonction ajouter le produit dans le local storage
+    const ajoutDansLocalStorage = ()=>{
+      //ajout de l'objet des valeurs sélectionnées par l'utilisateur dans un tableau
+      produitDansLocalStorage.push(userSelectProduit);
+
+      //conversion en JSON et stockage dans la clé "produit" du local storage
+      localStorage.setItem("produit", JSON.stringify(produitDansLocalStorage));
+    };
+
+    //fonction fenêtre de confirmation et choix aller sur page panier ou page d'acceuil
+    const pageSelect = ()=>{
+      if(window.confirm(`${canap.name}, ${colorProduit} a bien été ajouté au panier.
+Consulter le panier : OK ou revenir à l'acceuil : ANNULER.`)){
+        window.location.href = "cart.html";
+      }else{
+        window.location.href = "index.html";
+      }
+    }
+
+    //Si produits présents dans le local storage
+    if (produitDansLocalStorage) {
+      ajoutDansLocalStorage();
+      pageSelect();
+    }
+    //Pas de produits présents dans le local storage
+    else {
+      produitDansLocalStorage = [];
+      ajoutDansLocalStorage();
+      pageSelect();
+    }
+  });
 }
